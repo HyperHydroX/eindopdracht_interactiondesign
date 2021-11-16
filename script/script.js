da1f5cc8c1e14a15685931f19ff8f5af92a66afa
HyperHydroX <guillaumedebackere7@gmail.com>
2021-11-16 13:42:14 +0100

    gaan gebruik maken van mars weather api

--- script/script.js
+++ script/script.js
@@ -1,2 +1,4 @@
-const api = "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0";
-console.log(api);
+const get_api_data = (url) => fetch(url).then((r) => r.json());
+
+const endpoint = "https://api.nasa.gov/insight_weather/?api_key=iW652baPrAcb1T7No3bjFvxFMo5VnjFDYIcGpizl&feedtype=json&ver=1.0";
+console.log(get_api_data(endpoint));
