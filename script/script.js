4656d7c5dcd9f89916c6a871f156b1ca7706cb1d
HyperHydroX <guillaumedebackere7@gmail.com>
2021-11-18 16:06:36 +0100

    started using spaceX api en tested it

--- script/script.js
+++ script/script.js
@@ -1,4 +1,25 @@
 const get_api_data = (url) => fetch(url).then((r) => r.json());
 
-const endpoint = "https://api.nasa.gov/insight_weather/?api_key=iW652baPrAcb1T7No3bjFvxFMo5VnjFDYIcGpizl&feedtype=json&ver=1.0";
-console.log(get_api_data(endpoint));
+
+const show_rockets = (rockets) => {
+    // Listeners
+    const $rocket_name = document.querySelector(".js-rocket_name");
+    rockets.forEach(rocket => {
+        $rocket_name.textContent = rocket.name;
+    });
+};
+
+const get_api = async () => {
+    //url
+    const endpoint = "https://api.spacexdata.com/v4/rockets";
+    console.log(`Endpoint ${endpoint}`);
+    // data ophalen
+    const rocket_data = await get_api_data(endpoint);
+    console.log(rocket_data);
+
+    show_rockets(rocket_data);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    get_api();
+});
