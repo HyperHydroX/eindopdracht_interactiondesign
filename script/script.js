0d001a235a07e896fce8700be0fca22a580aa66f
HyperHydroX <guillaumedebackere7@gmail.com>
2021-11-19 11:19:28 +0100

    added rocket names

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
 const get_api_data = (url) => fetch(url).then((r) => r.json());
 
 
-const show_rockets = (rockets) => {
+const show_rockets = (rockets_data) => {
     // Listeners
     const $rocket_name = document.querySelector(".js-rocket_name");
-    rockets.forEach(rocket => {
-        $rocket_name.textContent = rocket.name;
+    const rockets = document.querySelector(".js-rockets");
+    console.log(rockets)
+    rockets_data.forEach(rocket_data => {
+        rockets.innerHTML += `<li class="c-rocket_name">${rocket_data.name}</li>`;
     });
+    // rockets.forEach(rocket => {
+    //     $rocket_name.textContent = rocket.name;
+    // });
 };
 
 const get_api = async () => {
